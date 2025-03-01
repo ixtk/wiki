0056f60737c8bba0dd5ce1316bad5495a171c6a3
ixtk <ikhcamot@gmail.com>
2025-03-01 18:57:23 +0400

    Add topic add page

--- frontend/src/pages/NewTopic.jsx
+++ frontend/src/pages/NewTopic.jsx
@@ -1,1 +1,52 @@
-export const NewTopic = () => <h1>New Topic</h1>
+import { useState } from "react"
+import { useNavigate } from "react-router"
+
+export const NewTopic = () => {
+  const navigate = useNavigate()
+  const [newTopic, setNewTopic] = useState({
+    title: "",
+    content: ""
+  })
+
+  const addNewTopic = async (event) => {
+    event.preventDefault()
+
+    const response = await fetch("http://localhost:3000/entries", {
+      method: "POST",
+      body: JSON.stringify(newTopic)
+    })
+
+    const json = await response.json()
+
+    if (response.ok) {
+      navigate(`/entries/${json.id}`)
+    }
+  }
+
+  return (
+    <>
+      <h2>Add a new topic</h2>
+      <form onSubmit={addNewTopic} className="new-topic">
+        <label>
+          Title
+          <input
+            onChange={(e) =>
+              setNewTopic({ ...newTopic, title: e.target.value })
+            }
+            type="text"
+          />
+        </label>
+        <label>
+          Content
+          <textarea
+            onChange={(e) =>
+              setNewTopic({ ...newTopic, content: e.target.value })
+            }
+            rows={5}
+          />
+        </label>
+        <button type="submit">Save</button>
+      </form>
+    </>
+  )
+}
